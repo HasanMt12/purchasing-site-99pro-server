1d238ed00abced2307539342fc6625d2f6fda531
Hasan Mt <hasan.2mahmud22@gmail.com>
2023-06-08 09:38:40 +0600

    feat: mongoDB connect and vercel deploy

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-
-
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+
+
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -9,6 +11,21 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gniuvqv.mongodb.net/?retryWrites=true&w=majority`;
+ console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const movieCollections = client.db('movieAppDB').collection('MoviesData')
+    
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
 
 
 app.get('/', async (req, res) => {
